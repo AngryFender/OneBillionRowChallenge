0013ef2b8dea953a05c92a5d26a8aadeb74433dc
AngryFender <Teabiscuitperson@gmail.com>
2025-12-19 23:51:14 +0000

    Adding new method for multithreading

--- main.cpp
+++ main.cpp
@@ -89,6 +89,70 @@ void mmap_method()
     close(fd);
 }
 
+void mmap_with_thread_method()
+{
+    int fd = open(DATA_FILE_PATH,O_RDONLY);
+
+    struct stat st;
+    fstat(fd,&st);
+    size_t size = st.st_size;
+
+    std::cout << "\nthe size of the file = " << std::to_string(size) << "\n";
+    void* addr = mmap(nullptr, size, PROT_READ, MAP_PRIVATE, fd, 0);
+
+    if(addr == reinterpret_cast<void*>(-1))
+    {
+        std::cerr << "failed registering mmap of the file: " << strerror(errno) << "\n";
+        return;
+    }
+
+    char *begin = static_cast<char*>(addr);
+    char *end = begin + size;
+    std::string_view view(begin, end);
+
+
+    std::pair<size_t, size_t> city{0, 0}; //first = starting pos, second = count of characters after first
+    std::pair<size_t, size_t> temp{0, 0}; //first = starting pos, second = count of characters after first
+
+    std::unordered_map<std::string_view, Data> map;
+
+    std::string_view value_view;
+    double value = 0;
+
+    for(size_t i = 0; i < size; ++i)
+    {
+        switch(view[i])
+        {
+        case ';':
+            {
+                city.second = i - city.first;
+                temp.first = i + 1;
+                break;
+            }
+        case '\n':
+            {
+                auto& [max, min,mean, count] = map.try_emplace(view.substr(city.first, city.second)).first->second;
+
+                // parse float using from_chars -> value
+                value_view = view.substr(temp.first, i-temp.first);
+                auto [ptr, ec] = std::from_chars(value_view.data(), value_view.data()+value_view.size(), value);
+
+                min = std::min(value, min);
+                max = std::max(value, max);
+                mean += (value - mean) / static_cast<double>(++count);
+
+                city.first = i + 1;
+                break;
+            }
+            default:break;
+        }
+    }
+
+    // cleanup
+    munmap(addr,size);
+    close(fd);
+}
+
 void mmap_flyweight_method()
 {
     int fd = open(DATA_FILE_PATH,O_RDONLY);
