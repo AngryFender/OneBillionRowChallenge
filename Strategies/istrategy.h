7f9790e5fdc4221874958bea3fc790ab62ccca5d
AngryFender <Teabiscuitperson@gmail.com>
2026-01-02 17:09:20 +0000

    Added new structs for preparation and result of the IStrategy logic execution

--- Strategies/istrategy.h
+++ Strategies/istrategy.h
@@ -1,11 +1,23 @@
 #ifndef ISTRATEGY_H
 #define ISTRATEGY_H
 
+struct StratInfo
+{
+    std::string_view view;
+    size_t file_size = 0;
+};
+
+struct StratResult
+{
+    std::string name{};
+    uint64_t total_lines = 0;
+};
+
 class IStrategy
 {
 public:
     virtual ~IStrategy() = default;
-    virtual void parse(std::string_view view) = 0;
+    virtual void parse(const StratInfo& data, StratResult& result) = 0;
 };
 
 #endif //ISTRATEGY_H
