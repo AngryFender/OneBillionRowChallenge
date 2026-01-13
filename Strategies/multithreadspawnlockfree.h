79d451604322ab171f86beebe9de85b7bd776fb2
AngryFender <Teabiscuitperson@gmail.com>
2026-01-13 20:34:47 +0000

    full implementation of the lock free approach by using vector of maps

--- Strategies/multithreadspawnlockfree.h
+++ Strategies/multithreadspawnlockfree.h
@@ -1,30 +1,30 @@
 #ifndef MULTITHREADSPAWNLOCKFREE_H
 #define MULTITHREADSPAWNLOCKFREE_H
 
-#include <charconv>
 #include <numeric>
 #include <string>
 #include <string_view>
 #include <thread>
 #include <unordered_map>
+#include <vector>
+
 #include "../data.h"
 #include "istrategy.h"
 
-class MultiThreadSpawn final: public IStrategy
+class MultiThreadSpawnLockFree final: public IStrategy
 {
 public:
-    explicit MultiThreadSpawn(const int num_of_threads = 0):_thread_no(num_of_threads){}
-    ~MultiThreadSpawn()  override = default;
+    explicit MultiThreadSpawnLockFree(const int num_of_threads = 0):_thread_no(num_of_threads){}
+    ~MultiThreadSpawnLockFree()  override = default;
 
     void parse(const StratInfo& data, StratResult& result) override
     {
-        /*
         result.name.append(std::to_string(_thread_no) + " Threads Spawn");
 
-        std::mutex mutex;
-        std::unordered_map<std::string_view, Data> map;
+        using map = std::unordered_map<std::string_view, Data>;
         std::vector<std::thread> thread_collection;
         thread_collection.reserve(_thread_no);
+        std::vector<map> maps;
 
         float high = 0;
         float low = -1;
@@ -35,6 +35,7 @@ public:
         for (int t = 0; t < _thread_no; ++t)
         {
             ranges.emplace_back(++low * factor, std::min(++high * factor, static_cast<float>(data.file_size)));
+            maps.push_back(std::unordered_map<std::string_view, Data>());
         }
 
         std::vector<uint64_t> line_count(_thread_no,0);
@@ -61,9 +62,7 @@ public:
                             temp.second = i - temp.first;
                             value = parse_value_view(view,temp);
                             {
-                                std::lock_guard lock(mutex);
-
-                                auto& [sum, max, min, count] = map.try_emplace(view.substr(city.first, city.second)).first->second;
+                                auto& [sum, max, min, count] = maps[t].try_emplace(view.substr(city.first, city.second)).first->second;
 
                                 min = std::min(value, min);
                                 max = std::max(value, max);
@@ -90,7 +89,6 @@ public:
         }
 
         result.total_lines = std::accumulate(line_count.begin(),line_count.end(),0);
-        */
     }
 private:
     int _thread_no = 0;
