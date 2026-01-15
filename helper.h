8642b7dec0be1481052efb52cb7738c4755fee85
AngryFender <Teabiscuitperson@gmail.com>
2026-01-15 23:29:35 +0000

    Added a check for eof

--- helper.h
+++ helper.h
@@ -33,9 +33,9 @@ inline uint32_t parse_value_str(const std::string& str)
     return value;
 }
 
-inline size_t find_eof(std::string_view view, size_t start)
+inline size_t find_eof(std::string_view view, size_t start, const size_t eof)
 {
-    while(view[start] != '\n')
+    while(view[start] != '\n' && eof > start+1)
     {
         ++start;
     }
