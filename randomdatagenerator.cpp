607c592d711888718e0dfabe8aa6fe86af89b83a
AngryFender <Teabiscuitperson@gmail.com>
2026-02-17 23:55:09 +0000

    Writing the random places with random temperature to the file until the desired line count is satisfied

--- randomdatagenerator.cpp
+++ randomdatagenerator.cpp
@@ -5,6 +5,7 @@
 #include <random>
 #include <bits/algorithmfwd.h>
 #include <algorithm>
+#include <sstream>
 
 bool RandomDataGenerator::generate(const uint32_t line_limit)
 {
@@ -13,7 +14,7 @@ bool RandomDataGenerator::generate(const uint32_t line_limit)
         return false;
     }
 
-    uint32_t line_counts = 0;
+    uint32_t current_line_counts = 0;
     std::unordered_set<std::string> setPlace;
     std::unordered_map<int, std::string> mapData;
     setPlace.reserve(10000);
@@ -30,7 +31,7 @@ bool RandomDataGenerator::generate(const uint32_t line_limit)
             setPlace.insert(place);
             mapData[++place_id] = place;
         }
-        ++line_counts;
+        ++current_line_counts;
     }
 
     std::random_device rd;
@@ -39,11 +40,29 @@ bool RandomDataGenerator::generate(const uint32_t line_limit)
     std::uniform_int_distribution<int> id_dist(1, place_id);
     std::normal_distribution<double> temp_dist(10.0, 10.0);
 
-    int random_id = id_dist(gen);
-    auto raw_temp = temp_dist(gen);
-    auto random_temp = std::clamp<double>(raw_temp, -20.0, 40.0);
+    _file.clear();
+    _file.seekp(0, std::ios::beg);
 
-    std::cout<<"random place id = "<< random_id << "random temp = "<<random_temp;
+    std::stringstream buffer;
+    buffer<<_file.rdbuf();
+    const std::string file_content = buffer.str();
+
+    const float target_multiple = static_cast<float>(line_limit) / static_cast<float>(current_line_counts);
+    int random_place_id = 0;
+    double raw_temp = 0.0;
+    double random_temp = 0.0;
+
+    for(float count = 1.0f ; count < target_multiple; ++count)
+    {
+         random_place_id = id_dist(gen);
+         raw_temp = temp_dist(gen);
+         random_temp = std::clamp<double>(raw_temp, -20.0, 40.0);
+        _file << mapData[random_place_id] <<";"<<random_temp<<"\n";
+    }
+
+    std::cout<<target_multiple<<","<<current_line_counts<<std::endl;
+    return true;
+
 
     return true;
 }
